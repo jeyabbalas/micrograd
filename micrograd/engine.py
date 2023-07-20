348c026e58062d31a1e7675b68cd20359ec79176
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2023-07-20 07:36:32 -0400

    Add additional operators

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -37,6 +37,7 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -46,7 +47,20 @@ class Value:
 
         return out
 
+    def __radd__(self, other):  # called when the left operand (other) does not support addition e.g. 3 + Value(2)
+        return self + other
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):  # called when the left operand (other) does not support subtraction e.g. 3 - Value(2)
+        return other + (-self)
+
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -56,6 +70,39 @@ class Value:
 
         return out
 
+    def __rmul__(self, other):  # called when the left operand (other) does not support multiplication e.g. 3 * Value(2)
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other ** -1
+
+    def __rtruediv__(self, other):  # called when the left operand (other) does not support division e.g. 3 / Value(2)
+        return other * self ** -1
+
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), 'exp')
+
+        def _backward():
+            self.grad += math.exp(self.data) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data ** other.data, (self, other), f'**{other.data}')
+
+        def _backward():
+            self.grad += other.data * self.data ** (other.data - 1) * out.grad
+            other.grad += math.log(self.data) * self.data ** other.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __rpow__(self, other):  # called when the left operand (other) does not support power e.g. 3 ** Value(2)
+        other = other if isinstance(other, Value) else Value(other)
+        return other ** self
+
     def tanh(self):
         x = self.data
         tanh_x = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
@@ -63,6 +110,28 @@ class Value:
 
         def _backward():
             self.grad += (1 - tanh_x**2) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        x = self.data
+        relu_x = max(0, x)
+        out = Value(relu_x, (self, ), 'relu')
+
+        def _backward():
+            self.grad += (x > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def sigmoid(self):
+        x = self.data
+        sigmoid_x = 1 / (1 + math.exp(-x))
+        out = Value(sigmoid_x, (self, ), 'sigmoid')
+
+        def _backward():
+            self.grad += sigmoid_x * (1 - sigmoid_x) * out.grad
         out._backward = _backward
 
         return out
